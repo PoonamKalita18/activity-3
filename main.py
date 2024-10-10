c9fb96a3971180370824e31a412ab99a0dc9c0de
PoonamKalita18 <arunakalita2015@gmail.com>
2024-10-10 22:01:45 +0530

    gngfhn

---
+++ main.py
@@ -0,0 +1,15 @@
+first_name = "Poonam"
+second_name = "Kalita"
+full_name = "Poonam Kalita"
+example ="Haa"*5
+
+print("First name is :", first_name)
+print("Last name is :", second_name)
+print("Full name is :", full_name)
+print("string multiplied 5 times gives result :" , example)
+
+word ="Coding"
+print("Length of word is :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:3])
